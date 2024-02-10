993c0c04f8413205c86cd6f41ab98dca3d43759f
stickburn <yashank@hey.com>
2024-02-10 22:44:09 +0530

    feat: save todo to indexdb

--- sunlit_fe/src/tmp/database.tsx
+++ sunlit_fe/src/tmp/database.tsx
@@ -1,3 +1,5 @@
+import { format } from 'date-fns';
+
 const dbName = 'sunlit_db';
 const storeName = 'journals';
 
@@ -20,5 +22,32 @@ function openJournalsDb(): Promise<IDBDatabase> {
     request.onsuccess = () => {
       resolve(request.result);
     };
+  });
+}
+
+export async function postJournalDb(value: string) {
+  const dateNow = Date.now();
+  const journal = { created_at: format(dateNow, 'yyyy-MM-dd'), uuid: dateNow, notes: value };
+  const db = await openJournalsDb();
+  const tx = db.transaction(storeName, 'readwrite');
+  const store = tx.objectStore(storeName);
+
+  return new Promise((resolve, reject) => {
+    const request = store.put(journal);
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
+
+export async function getDb(key: string): Promise<string> {
+  const db = await openJournalsDb();
+  const tx = db.transaction(storeName, 'readonly');
+  const store = tx.objectStore(storeName);
+
+  const request = store.get(key);
+
+  return new Promise((resolve, reject) => {
+    (request.onsuccess = () => resolve(request.result ? request.result.notes : null)),
+      (request.onerror = () => reject(request.error));
   });
 }
