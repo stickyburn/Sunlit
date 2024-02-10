09d1ee4b98debad338684aec89c91236df4fd8e2
stickburn <yashank@hey.com>
2024-02-11 02:57:53 +0530

    feat: toggle theme

--- sunlit_fe/src/components/Homepad/Editor.tsx
+++ sunlit_fe/src/components/Homepad/Editor.tsx
@@ -1,61 +1,18 @@
 /* eslint-disable solid/no-innerhtml */
-import { Component, Match, Switch, createEffect, createSignal, onCleanup, onMount } from 'solid-js';
+import { Component, Match, Switch, createEffect, createSignal, onCleanup } from 'solid-js';
 import { marked } from 'marked';
-import * as DB from '../../tmp/database';
-import { format } from 'date-fns';
+import { isEditing } from '~/stores/editState';
 
 interface EditorProps {
   date?: Date;
   title?: string;
+  content: () => string;
+  handleInput: (e: InputEvent) => void;
+  handleToggleEdit: (e: KeyboardEvent) => void;
 }
 
 const Editor: Component<EditorProps> = (props) => {
-  const [content, setContent] = createSignal('');
-  const [tags, setTags] = createSignal<string[]>([]);
-  const [isEditing, toggleIsEditing] = createSignal(false);
   const [editorRef, setEditorRef] = createSignal<HTMLTextAreaElement | null>(null);
-
-  const saveContentToDB = async () => {
-    await DB.postJournalDb(content());
-  };
-
-  const toggleEdit = () => {
-    if (isEditing()) {
-      saveContentToDB();
-    }
-    toggleIsEditing(!isEditing());
-  };
-
-  const updateTagsFromContent = (textContent: string) => {
-    // tags are #example or #example-tag
-    // remove ##.. but include everything else
-    const tagPattern = /#[^\s#]+/g;
-    const foundTags = textContent.match(tagPattern) || [];
-    setTags(foundTags);
-  };
-
-  const handleInput = (e: InputEvent) => {
-    const target = e.target as HTMLTextAreaElement;
-    setContent(target.value);
-    updateTagsFromContent(target.value);
-  };
-
-  const handleToggleEdit = (e: KeyboardEvent) => {
-    if (e.ctrlKey && e.key === 'e') {
-      e.preventDefault();
-      toggleEdit();
-    }
-  };
-
-  onMount(() => {
-    const loadDataFromDb = async () => {
-      const data = await DB.getDb(props.date?.toString() || format(Date.now(), 'yyyy-MM-dd'));
-      setContent(data || '');
-      updateTagsFromContent(data || '');
-    };
-
-    loadDataFromDb();
-  });
 
   createEffect(() => {
     if (isEditing() && editorRef()) {
@@ -63,35 +20,24 @@ const Editor: Component<EditorProps> = (props) => {
     }
   });
 
-  window.addEventListener('keydown', handleToggleEdit);
-  onCleanup(() => window.removeEventListener('keydown', handleToggleEdit));
+  window.addEventListener('keydown', props.handleToggleEdit);
+  onCleanup(() => window.removeEventListener('keydown', props.handleToggleEdit));
 
   return (
-    <div class="py-6 px-12">
-      <div class="flex justify-between mb-4">
-        <h2>{format(props?.date || Date.now(), 'MMM dd, yyyy')}</h2>
-        <button class="text-xl min-w-36 rounded-md outline outline-1" onClick={toggleEdit}>
-          {isEditing() ? 'Preview' : 'Edit'}
-        </button>
-      </div>
-      <Switch fallback={<div>Loading</div>}>
-        <Match when={isEditing()}>
-          <textarea
-            class="w-full h-96 mt-4 p-2 text-white text-lg outline outline-1 outline-text-dark bg-background-dark"
-            onInput={handleInput}
-            ref={setEditorRef}
-            value={content()}
-          />
-        </Match>
+    <Switch fallback={<div>Loading</div>}>
+      <Match when={isEditing()}>
+        <textarea
+          class="w-full h-96 p-2 text-primary text-lg outline outline-1 outline-primary bg-background rounded-lg"
+          onInput={(e) => props.handleInput(e)}
+          ref={setEditorRef}
+          value={props.content()}
+        />
+      </Match>
 
-        <Match when={!isEditing()}>
-          <div
-            class="w-full h-full p-2 rounded-md"
-            innerHTML={marked.parse(content()).toString()}
-          />
-        </Match>
-      </Switch>
-    </div>
+      <Match when={!isEditing()}>
+        <div class="w-full h-full" innerHTML={marked.parse(props.content()).toString()} />
+      </Match>
+    </Switch>
   );
 };
 
